a1875a1b521caa443afb2d9a009209a9cae298b4
Виктор Бовыкин <aimsscs@gmail.com>
2025-07-06 00:55:35 +0300

    Add docs

--- app/schemas/wallet_schemas.py
+++ app/schemas/wallet_schemas.py
@@ -6,23 +6,41 @@ from pydantic import BaseModel, condecimal
 
 
 class OperationType(Enum):
+    """
+    Enum representing the types of the wallet operations
+    """
     DEPOSIT = "DEPOSIT"
     WITHDRAW = "WITHDRAW"
 
 
 class WalletResponse(BaseModel):
+    """
+    Response model representing a wallet`s public data
+    """
     wallet_id: str
     balance: Annotated[Decimal, condecimal(gt=0, max_digits=12, decimal_places=2)]
 
 
 class BaseModelWallet(BaseModel):
+    """
+    Base model for wallet-related requests containing an amount field.
+    The amount must be positive decimal with up to 12 digits and 2 decimal places.
+    Extra fields are forbidden to avoid unexpected inputs.
+    """
     model_config = {"extra": "forbid"}
     amount: Annotated[Decimal, condecimal(gt=0, max_digits=12, decimal_places=2)]
 
 
 class OperationModel(BaseModelWallet):
+    """
+    Model representing a wallet operation request.
+    Includes the operation types and an amount value.
+    """
     operation_type: OperationType
 
 
 class WalletCreateModel(BaseModelWallet):
+    """
+    Model for creating a new wallet with an initial amount value.
+    """
     pass
