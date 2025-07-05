6a7a8cd72c6094280df43b87e5ead5f13ebd1ed8
Виктор Бовыкин <aimsscs@gmail.com>
2025-07-06 00:35:21 +0300

    create custom exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException
+
+
+class WalletNotFoundException(HTTPException):
+    def __init__(self, detail: str = 'Wallet Not Found') -> None:
+        super().__init__(status_code=404, detail=detail)
+
+
+class InvalidOperationException(HTTPException):
+    def __init__(self, detail: str = 'Invalid Operation Type') -> None:
+        super().__init__(status_code=400, detail=detail)
+
+
+class OperationExecutionException(HTTPException):
+    def __init__(self, detail: str) -> None:
+        super().__init__(status_code=400, detail=detail)
