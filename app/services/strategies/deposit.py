9d38df3988fb930eb246e98c63c530cf91c50a9b
Виктор Бовыкин <aimsscs@gmail.com>
2025-07-06 00:42:12 +0300

    Change docs

--- app/services/strategies/deposit.py
+++ app/services/strategies/deposit.py
@@ -4,5 +4,16 @@ from app.services.strategies.base import OperationStrategyAbstract
 
 
 class DepositStrategy(OperationStrategyAbstract):
+    """
+    Strategy for handling operation deposit on a wallet
+    """
     def execute(self, wallet: Wallet, amount: Decimal) -> None:
+        """
+        Execute a deposit on the given wallet with the specified amount.
+        Args:
+            wallet (Wallet): The wallet instance on which the operation is performed.
+            amount (float): The amount involved in the operation.
+        """
+        if amount < 1:
+            raise ValueError('Amount must be greater than zero')
         wallet.deposit(amount=amount)
--- app/services/strategies/withdraw.py
+++ app/services/strategies/withdraw.py
@@ -4,5 +4,16 @@ from app.services.strategies.base import OperationStrategyAbstract
 
 
 class WithDrawStrategy(OperationStrategyAbstract):
+    """
+    Strategy for handling withdraw operation on a wallet
+    """
     def execute(self, wallet: Wallet, amount: Decimal) -> None:
+        """
+        Execute withdraw operation on the given wallet with the specified amount.
+        Args:
+            wallet (Wallet): The wallet instance on which the operation is performed.
+            amount (float): The amount involved in the operation.
+        """
+        if amount < 1:
+            raise ValueError('Amount must be greater than zero')
         wallet.withdraw(amount=amount)
