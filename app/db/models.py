3e45874d479a226311a5bc5a743fbbb100870789
Виктор Бовыкин <aimsscs@gmail.com>
2025-07-06 00:48:36 +0300

    Add docs

--- app/db/models.py
+++ app/db/models.py
@@ -10,6 +10,14 @@ class Base(DeclarativeBase):
 
 
 class Wallet(Base):
+
+    """
+    Database model represent a user`s wallet
+    Attributes:
+        uuid (str): Unique UUID of the wallet
+        balance (Decimal): Current balance of the wallet, with precision up to 2 decimal places
+    """
+
     __tablename__ = "wallets"
 
     uuid: Mapped[str] = mapped_column(
@@ -22,9 +30,19 @@ class Wallet(Base):
     balance: Mapped[Decimal] = mapped_column(Numeric(12, 2), default=0)
 
     def deposit(self, amount: Decimal) -> None:
+        """
+        Increases balance of the wallet by the specified amount
+        Args:
+            amount (Decimal): Amount to add to the balance (must be positive)
+        """
         self.balance += amount
 
     def withdraw(self, amount: Decimal) -> None:
+        """
+        Decreases balance of the wallet by the specified amount.
+        Args:
+            amount (Decimal): Amound to subtrack from the balance (must be also positive)
+        """
         if self.balance < amount:
             raise ValueError("Insufficient funds")
         self.balance -= amount
