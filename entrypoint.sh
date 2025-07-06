b9981fb0de78ffd87ce7b7b4e2fc7ecb95f0ad45
Виктор Бовыкин <aimsscs@gmail.com>
2025-07-06 21:11:08 +0300

    Add beautiful logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "Start migration"
+set -e
+
+echo
+echo "======================================="
+echo ">>> Start migration <<<"
+echo "======================================="
+echo
 
 uv run alembic upgrade head
 
-echo "Start server"
+echo
+echo
+echo ">>> Migration completed <<<"
+echo
+echo
+
+echo
+echo "======================================="
+echo ">>> Start server <<<"
+echo "======================================="
+echo
 
 uv run uvicorn main:app --host 0.0.0.0 --port 8000
